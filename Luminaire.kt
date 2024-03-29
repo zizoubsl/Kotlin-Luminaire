e278dcc1c5c3b9fe98e1f1c709ae368488afdcb8
Zyad <bennzya7@gmail.com>
2024-03-29 12:02:14 +0100

    update luminaire

--- Luminaire.kt
+++ Luminaire.kt
@@ -5,18 +5,42 @@ class Luminaire : Eclairage {
     val ampoule4 = AmpouleIncandescente()
     constructor()
     override fun allumer() {
-        TODO("Not yet implemented")
+       ampoule1.allumer()
+        ampoule2.allumer()
+        ampoule3.allumer()
+        ampoule4.allumer()
     }
     override fun eteindre() {
-        TODO("Not yet implemented")
+        ampoule1.eteindre()
+        ampoule2.eteindre()
+        ampoule3.eteindre()
+        ampoule4.eteindre()
     }
     override fun intensifier() {
-        TODO("Not yet implemented")
+        ampoule1.intensifier()
+        ampoule2.intensifier()
+        ampoule3.intensifier()
+        ampoule4.intensifier()
     }
     override fun diminuer() {
-        TODO("Not yet implemented")
+        ampoule1.diminuer()
+        ampoule2.diminuer()
+        ampoule3.diminuer()
+        ampoule4.diminuer()
     }
     override fun etat(): Int {
-        TODO("Not yet implemented")
+        var etatGlobal = 0
+        if (ampoule1.etat == -1 && ampoule2.etat == -1 && ampoule3.etat == -3 && ampoule4.etat == -1)
+            etatGlobal = -1
+
+        else{ etatGlobal = 0
     }
+        return etatGlobal
+    }
+
+    override fun toString(): String {
+        return "Luminaire(ampoule1=$ampoule1, ampoule2=$ampoule2, ampoule3=$ampoule3, ampoule4=$ampoule4)"
+    }
+
+
 }
